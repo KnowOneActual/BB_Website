9c3da1a0fc428660938030e0abb3906fd60fdea9
BeauBremer <6852572+KnowOneActual@users.noreply.github.com>
2025-06-04 14:04:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Easter Egg functions
+// Fun functions
 const egg = () => {
   console.log("%c👋 Well hey there, curious mind! You found the hidden message! 🎉", "color: fuchsia; font-size: 1.5rem;");
   // Using a custom modal-like message instead of alert() for better user experience.

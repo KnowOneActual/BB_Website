a0ca69608aa9f6549338cfccb96d69c0510da0ba
Beau Bremer <beau.bremer@gmail.com>
2025-09-11 19:37:11 -0500

    chore: update tailwind config and files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // This tells Tailwind to scan all of your HTML and JavaScript files for classes.
+    "./*.html",
+    "./**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

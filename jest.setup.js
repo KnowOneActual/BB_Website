1f524ca8d1d17993d775e43df7c2bae137a83979
Beau Bremer <beau.bremer@gmail.com>
2025-11-02 13:36:49 -0600

    updated css and added test files

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+// Jest setup file for global test configuration
+// This file runs before each test file
+
+// Add custom matchers if needed
+// expect.extend({...});
+
+// Global test utilities can be added here
